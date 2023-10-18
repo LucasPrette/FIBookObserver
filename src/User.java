5d5f6aa5a167aeca42ab6daa65f3d52b4e97507b
LucasPrette <lucassprette@hotmail.com>
2023-10-18 11:54:56 -0300

    feat: user class

---
+++ src/User.java
@@ -0,0 +1,12 @@
+public class User implements Observer{
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    };
+
+    @Override
+    public void notifi() {
+
+    };
+}
