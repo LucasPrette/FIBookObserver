b3296d7389f9de7ebe9287e984ad1124aa09ce28
LucasPrette <lucassprette@hotmail.com>
2023-10-18 12:42:23 -0300

    feat: user notify create post

--- src/User.java
+++ src/User.java
@@ -1,12 +1,33 @@
+import javax.security.auth.Subject;
+import java.util.ArrayList;
+
 public class User implements Observer{
     private String name;
+    private ArrayList<User> followers;
 
     public User(String name) {
         this.name = name;
+        this.followers = new ArrayList<>();
     };
+
+    public void addFollower(User user) {
+        followers.add(user);
+    }
+
+    public void removeFollower(User user) {
+        followers.remove(user);
+    }
+    
+    public void createPost(String subject, User author) {
+        var post = new Post(subject, author);
+        notifyObserver(author);
+    }
 
     @Override
-    public void notifi() {
+    public void notifyObserver(User author) {
+        System.out.println();
+    }
 
-    };
+
+
 }
